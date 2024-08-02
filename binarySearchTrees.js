9c88b401338252df17a9d276bb06df64e7a4d85f
catalinbroinas <catalinbroinas@yahoo.com>
2024-08-02 17:19:46 +0300

    Add `buildTree` method to `Tree` * Convert an array to a balanced BST

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -8,8 +8,53 @@ function CreateNode(data, left = null, right = null) {
     };
 }
 
-function Tree(root = null) {
+function Tree() {
     const { sortArray, removeDuplicate } = ArrayMethod();
+
+    // Convert the sorted, unique array to a balanced BST
+    const buildTree = (arr) => {
+        // Check if the array is valid
+        if (!Array.isArray(arr) || !arr.length) {
+            return null;
+        }
+
+        // Sort and remove duplicates
+        const treeArr = sortArray(removeDuplicate(arr));
+
+        // Initialize the root node with the middle element
+        const mid = Math.floor(treeArr.length / 2);
+        const root = CreateNode(treeArr[mid]);
+
+        // Initialize the queue with the root node and the initial subarray ranges
+        const q = [[root, [0, mid - 1]], [root, [mid + 1, treeArr.length - 1]]];
+
+        while (q.length > 0) {
+            const [parent, [left, right]] = q.shift();
+
+            // Process subarrays
+            if (left <= right) {
+                const mid = Math.floor((left + right) / 2);
+                const child = CreateNode(treeArr[mid]);
+
+                // Attach the child to the parent node
+                if (treeArr[mid] < parent.data) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+
+                // Add child nodes and their respective subarray ranges to the queue
+                q.push([child, [left, mid - 1]]);
+                q.push([child, [mid + 1, right]]);
+            }
+        }
+
+        return root;
+    };
+
+    return {
+        buildTree
+    };
 }
 
 export { Tree };
--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import { Tree } from "./binarySearchTrees.js";
+
+const { buildTree } = Tree();
+
+const myArr = [1, 7, 4, 9, 2, 3, 1, 2, 3, 4, 5, 6, 6, 6, 21, 12];
+console.log(buildTree(myArr));
